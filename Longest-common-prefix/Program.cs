cddff0c8bce4e922699b5dd06fe8d26932e8e0cb
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 03:47:07 +0200

    Добавьте файлы проекта.

---
+++ Longest-common-prefix/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0) return "";
+
+        // Ініціалізуємо префікс першим рядком
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            // Скорочуємо префікс, доки він не стане спільним з поточним рядком
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "") return ""; // якщо префікс став порожнім, вихід
+            }
+        }
+
+        return prefix;
+    }
+}
